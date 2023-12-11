5fbdcb6242e799eeafead4681b5323a7596c9310
João Alves <joao.2003.couto@gmail.com>
2023-12-11 14:25:52 +0000

    Day 8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use num_integer::lcm;
 advent_of_code::solution!(8);
 
 struct Node {
@@ -45,8 +46,51 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(steps)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut map: HashMap<String, Node> = HashMap::new();
+
+    let mut lines = input.lines();
+    let instructions: Vec<char> = lines.next().unwrap().chars().collect();
+    let mut raw_nodes = lines.skip(1);
+
+    for raw_node in raw_nodes {
+        let (name, directions) = parse_node(raw_node);
+        map.insert(name, directions);
+    }
+
+    let mut current_nodes: Vec<String> = map
+        .keys().cloned().collect::<Vec<String>>()
+        .into_iter().filter(|x| x.ends_with("A")).collect();
+
+    let mut steps: Vec<u32> = Vec::new();
+    let mut answer = 0;
+    let mut index = 0;
+
+    for node in current_nodes {
+        let mut _steps = 0;
+        let mut current_node = node;
+
+        loop {
+            let instruction = *instructions.get(index).unwrap();
+            let _node = map.get(&current_node).unwrap().clone();
+
+            current_node = if instruction == 'L' { _node.left.to_string() } else { _node.right.to_string() };
+
+            if index == instructions.len() - 1 { index = 0 } else { index += 1 }
+            _steps += 1;
+
+            if current_node.ends_with("Z") {
+                print!("");
+                break;
+            }
+        }
+
+        steps.push(_steps);
+    }
+
+    answer = steps.iter().cloned().fold(steps[0] as u64, |acc, x| lcm(acc, x as u64));
+
+    Some(answer)
 }
 
 #[cfg(test)]
@@ -62,6 +106,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
